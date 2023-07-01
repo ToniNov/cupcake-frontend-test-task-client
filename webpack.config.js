e1fc43c73f852638cceb23fb0c434ffbb8de8f83
ToniNov <antoni.novik@yandex.ru>
2023-07-01 18:47:02 +0300

    fix: webpack && useLongpolling

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
       filename: '[name].[contenthash].css',
       chunkFilename: '[id].css',
     }),
-    new CleanWebpackPlugin,
+    new CleanWebpackPlugin(),
     new webpack.ProvidePlugin({
       React: 'react',
     }),
